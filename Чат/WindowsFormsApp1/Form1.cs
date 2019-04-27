e62720973a0a6749f50a308708725ca793ba970d
damirs11 <damir.calixov1@gmail.com>
2019-04-27 13:10:00 +0500

    chatr

---
+++ Чат/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool alive = false; 
+        UdpClient client;
+        const int LOCALPORT = 5000; 
+        const int REMOTEPORT = 6000; 
+        const int TTL = 20;
+        const string HOST = "235.5.5.1"; 
+        IPAddress groupAddress;
+        IPEndPoint localpt = new IPEndPoint(IPAddress.Any, REMOTEPORT);
+        string userName; 
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.Enabled = true; 
+            button2.Enabled = false;
+            button3.Enabled = false; 
+            textBox2.ReadOnly = true; 
+
+            groupAddress = IPAddress.Parse(HOST);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            userName = textBox1.Text;
+            textBox1.ReadOnly = true;
+
+            try
+            {
+                client = new UdpClient(REMOTEPORT);
+                // присоединяемся к групповой рассылке
+                client.JoinMulticastGroup(groupAddress, TTL);
+
+                // запускаем задачу на прием сообщений
+                Task receiveTask = new Task(ReceiveMessages);
+                receiveTask.Start();
+
+                // отправляем первое сообщение о входе нового пользователя
+                string message = userName + " вошел в чат";
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                client.Send(data, data.Length, HOST, REMOTEPORT);
+
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                client = new UdpClient(LOCALPORT);
+                // присоединяемся к групповой рассылке
+                client.JoinMulticastGroup(groupAddress, TTL);
+
+                // запускаем задачу на прием сообщений
+                Task receiveTask = new Task(ReceiveMessages);
+                receiveTask.Start();
+
+                // отправляем первое сообщение о входе нового пользователя
+                string message = userName + " вошел в чат";
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                client.Send(data, data.Length, HOST, REMOTEPORT);
+
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+            }
+        }
+
+        private void ReceiveMessages()
+        {
+            alive = true;
+            try
+            {
+                while (alive)
+                {
+                    IPEndPoint remoteIp = null;
+                    byte[] data = client.Receive(ref remoteIp);
+                    string message = Encoding.Unicode.GetString(data);
+
+                    // добавляем полученное сообщение в текстовое поле
+                    this.Invoke(new MethodInvoker(() =>
+                    {
+                        string time = DateTime.Now.ToShortTimeString();
+                        textBox2.Text = time + " " + message + "\r\n" + textBox2.Text;
+                    }));
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                if (!alive)
+                    return;
+                throw;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string message = String.Format("{0}: {1}", userName, textBox3.Text);
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                client.Send(data, data.Length, HOST, REMOTEPORT);
+                textBox3.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string message = userName + " покидает чат";
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            client.Send(data, data.Length, HOST, REMOTEPORT);
+            client.DropMulticastGroup(groupAddress);
+
+            alive = false;
+            client.Close();
+
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (alive)
+                button2_Click(sender, e);
+        }
+    }
+}
